eec6fc9c72ad3b33a8a291a681a652a7405f9106
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-05-17 12:35:08 +0200

    Created drumpad.unity and made walls reactive to collisions

---
+++ Assets/DetectCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectCollision : MonoBehaviour {
+
+	public Color hitColor;
+	public Color defaultColor;
+
+	void OnCollisionEnter() {
+		gameObject.GetComponent<Renderer> ().material.color = hitColor;
+	}
+
+	void OnCollisionExit() {
+		gameObject.GetComponent<Renderer> ().material.color = defaultColor;
+	}
+
+
+}
