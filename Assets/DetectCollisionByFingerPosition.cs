f5fd9310a29ab8a0568d568c5b47d96898cb1bd6
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-05-18 22:40:34 +0200

    Using finger position to show feedback

---
+++ Assets/DetectCollisionByFingerPosition.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Leap;
+using System.Linq;
+
+public class DetectCollisionByFingerPosition : MonoBehaviour {
+	
+	//HandModel hand_model;
+	//Hand leap_hand;
+
+	public HandController handController;
+	public GameObject leftWall;
+	public GameObject rightWall;
+	public GameObject floor;
+	public Color hitColor;
+	public Color leftWallColor;
+	public Color rightWallColor;
+	public Color floorColor;
+
+	private HandModel[] hands;
+	private List<float> fingerXPositions = new List<float>();
+	private List<float> fingerYPositions = new List<float>();
+	private bool handInLeftWall = false;
+	private bool handInRightWall = false;
+	private bool handInFloor = false;
+
+	void Start () {
+
+		//HandController handController = GameObject.Find("HandControllerSandBox").transform.getComponent<HandController>();
+		//hands = handController.GetAllGraphicsHands();
+
+		/*
+		hand_model = GetComponent<HandModel>();
+		leap_hand = hand_model.GetLeapHand();
+		if (leap_hand == null) Debug.LogError("No leap_hand founded");*/
+
+		/*
+		List<Finger> fingers = leap_hand.Fingers;
+		foreach(Finger finger in fingers){
+		}*/
+	}
+	
+
+	void Update () {
+
+		hands = handController.GetAllGraphicsHands();
+
+		foreach(HandModel hand in hands) {
+
+			//get the x and y position of each finger and add it to the list
+			for (int i = 0; i < HandModel.NUM_FINGERS;i++) {
+
+				FingerModel finger = hand.fingers[i];
+
+				//add this finger x pos to the list in position i
+				float fingerXPos = finger.GetTipPosition().x;
+				fingerXPositions.Insert(i, fingerXPos);
+				//Debug.Log("xpos finger " + i + ": " + fingerXPositions[i]);
+
+				//add this finger y pos to the list in position i
+				float fingerYPos = finger.GetTipPosition().y;
+				fingerYPositions.Insert(i, fingerYPos);
+			}
+		}
+
+		//find the lowest value of finger x positions
+		float lowestXPos = Mathf.Min(fingerXPositions[0],fingerXPositions[1],fingerXPositions[2],fingerXPositions[3],fingerXPositions[4]);
+		//Debug.Log("lowestXPos: " + lowestXPos);
+
+		//if a finger entered the left wall
+		if(lowestXPos <= -4.0f && handInLeftWall == false) {
+			handInLeftWall = true;
+			StartCoroutine(FadeColor(leftWall,leftWallColor));
+		}
+
+		if (lowestXPos > -4.0f) {
+			handInLeftWall = false;
+		}
+
+		//find the highest value of finger x positions
+		float highestXPos = Mathf.Max(fingerXPositions[0],fingerXPositions[1],fingerXPositions[2],fingerXPositions[3],fingerXPositions[4]);
+		//Debug.Log("highestXPos: " + highestXPos);
+
+		if (highestXPos >= 4.0f && handInRightWall == false) {
+			handInRightWall = true;
+			StartCoroutine(FadeColor(rightWall,rightWallColor));
+		}
+
+		if (highestXPos < 4.0f) {
+			handInRightWall = false;
+		}
+
+		//find the lowest value of finger y positions
+		float lowestYPos = Mathf.Min(fingerYPositions[0],fingerYPositions[1],fingerYPositions[2],fingerYPositions[3],fingerYPositions[4]);
+		//Debug.Log("lowestYPos: " + lowestYPos);
+		
+		//if a finger entered the left wall
+		if(lowestYPos <= -1.2f && handInFloor == false) {
+			handInFloor = true;
+			StartCoroutine(FadeColor(floor,floorColor));
+		}
+		
+		if (lowestYPos > -1.2f) {
+			handInFloor = false;
+		}
+
+	}
+
+
+	public IEnumerator FadeColor(GameObject wall, Color fadeToColor) {
+		Debug.Log("Starting color lerp");
+		float ElapsedTime = 0.0f;
+		float TotalTime = 0.2f;
+		while (ElapsedTime < TotalTime) {
+			ElapsedTime += Time.deltaTime;
+			wall.GetComponent<Renderer>().material.color = Color.Lerp(hitColor, fadeToColor, (ElapsedTime / TotalTime));
+			yield return null;
+		}
+		Debug.Log("Ending color lerp");
+	}
+}
--- Assets/Scripts/DetectCollision.cs
+++ Assets/Scripts/DetectCollision.cs
@@ -6,12 +6,81 @@ public class DetectCollision : MonoBehaviour {
 	public Color hitColor;
 	public Color defaultColor;
 
+	private Color currentColor;
+	private Color nextColor;
+
+	private bool gettingFaded = false;
+	private bool handIsInWall;
+
+
+	void Start() {
+	
+	}
+
+	void FixedUpdate() {
+		gettingFaded = false;
+	}
+
+	/*
+	void Update() {
+
+		if (hitHappened == true) {
+
+			Debug.Log("triggered");
+
+			gameObject.GetComponent<Renderer> ().material.color = Color.Lerp(hitColor, defaultColor, 1.0f);
+			hitHappened = false;
+		}
+
+
+	}*/
+
+	/*
 	void OnCollisionEnter() {
 		gameObject.GetComponent<Renderer> ().material.color = hitColor;
 	}
 
 	void OnCollisionExit() {
 		gameObject.GetComponent<Renderer> ().material.color = defaultColor;
+	}*/
+
+	void OnTriggerEnter() {
+		Debug.Log("entered trigger");
+		//gameObject.GetComponent<Renderer> ().material.color = hitColor;
+		//hitHappened = true;
+
+		//if it's not already fading, start a fade
+		if (gettingFaded == false) {
+			gettingFaded = true;
+			StartCoroutine(FadeColor());
+		}
+
+	}
+	
+	void OnTriggerExit() {
+		//gameObject.GetComponent<Renderer> ().material.color = defaultColor;
+		//gettingFaded = false;
+	}
+
+	void OnTriggerStay() {
+		//gameObject.GetComponent<Renderer> ().material.color = hitColor;
+		//the hand is in the trigger so it should already be fading
+		gettingFaded = true;
+	}
+
+
+	public IEnumerator FadeColor() {
+		Debug.Log("Starting color lerp");
+		float ElapsedTime = 0.0f;
+		float TotalTime = 0.4f;
+		while (ElapsedTime < TotalTime) {
+			ElapsedTime += Time.deltaTime;
+			gameObject.GetComponent<Renderer>().material.color = Color.Lerp(hitColor, defaultColor, (ElapsedTime / TotalTime));
+			yield return null;
+		}
+		Debug.Log("Ending color lerp");
+		//done fading
+		gettingFaded = false;
 	}
 
 
