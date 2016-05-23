c00e1e24213de0aaa9bf43864a27138842522d8c
GeoffreyCooper <geoffrey.cooper@gmail.com>
2016-05-23 17:39:07 +0200

    Added metronome visualization

---
+++ Assets/Metronome2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Metronome2 : MonoBehaviour {
+
+
+	public Transform startMarker;
+	public Transform endMarker;
+	public float time;
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.R))
+			StartCoroutine(MoveOverTime());
+		
+	}
+	
+	IEnumerator MoveOverTime() {
+		while(true) {
+			float rate = 1.0f/time;
+			float index = 0.0f;
+			while( index < 1.0f )
+			{
+				transform.position = Vector3.Lerp( startMarker.position, endMarker.position, index );
+				index += rate * Time.deltaTime;
+				yield return null;
+			}
+			transform.position = endMarker.position;
+			//start over
+			index = 0.0f;
+		}
+	}
+}
